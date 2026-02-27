bce95a9f0652ada2e59cceeea0cad4a2119c28cb
giasas <giasas@users.noreply.github.com>
2026-02-27 13:24:46 +0200

    Clean up page.tsx by removing comments and formattingAdd copyright header to app/page.tsx
    Removed copyright comment and adjusted formatting.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import MultiCollateralForm from "@/components/multi-collateral-form";
+/**
+ * Copyright (C) 2024 Angelos Sfakianakis
+ * Licensed under the GNU General Public License v3.0
+ * For commercial licensing inquiries, contact: info@icida.com.cy
+ */
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-100 p-6">
-      <MultiCollateralForm />
-    </main>
-  );
-}
+import MultiCollateralForm from "@/components/multi-collateral-form";export default function Home() {  return (    <main className="flex min-h-screen items-center justify-center bg-gray-100 p-6">      <MultiCollateralForm />    </main>  );}
